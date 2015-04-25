7b2709f64c93dde3f7099fea4f3ca21f0ed3ab47
Mathias Paumgarten <mail@mathias-paumgarten.com>
2015-04-24 17:18:20 -0700

    transfer offices - need to commit

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,9 +1,16 @@
-module.exports = {
+var dat = require( "dat-gui" );
+
+var gui = new dat.GUI();
+var exports = module.exports = {
     size: 5,
     speed: 0.1,
     gridWidth: 106,
     gridHeight: 106,
     hyperlapse: true,
     canvasPadding: 5,
-    cyclesPerFrame: 5
+    cyclesPerFrame: 5,
+    message: "GRID"
 };
+
+gui.add( exports, "message" );
+
